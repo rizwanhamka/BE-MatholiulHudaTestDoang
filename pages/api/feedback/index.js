3ae3c2a676172b1c60b3a03fe9b11f12a8849863
rizwanhamka <ahmadrizwanhamka@mail.ugm.ac.id>
2025-12-03 21:22:05 +0700

    aaa

--- pages/api/feedback/index.js
+++ pages/api/feedback/index.js
@@ -1,5 +1,4 @@
 import prisma from "../../../lib/prisma";
-
 export default async function handler(req, res) {
   if (req.method === "GET") {
     const items = await prisma.feedback.findMany({
--- pages/api/kepengurusan/index.js
+++ pages/api/kepengurusan/index.js
@@ -1,5 +1,4 @@
 import prisma from "../../../lib/prisma";
-
 export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).end();
   const items = await prisma.kepengurusan.findMany({
